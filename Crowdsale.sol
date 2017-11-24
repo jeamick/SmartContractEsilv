2872236df0529130700215ca24b40dba9ffdbac7
jeamick <nounahon.j@gmail.com>
2017-11-24 15:15:05 +0100

    Update Crowdsale.sol

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.18;
+
 /// @title Solidity Smart-contract in order to manage a Crowdsale ICO
 /// @author Jean-Mickael Nounahon - <nounahon.j@gmail.com>
 /// @author Hugo Brunet- <Hugo.brunet.j@devinci.fr>
