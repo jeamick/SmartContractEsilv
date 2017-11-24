78efe1892ac15f1c90e2677fb80222e0245669ac
jeamick <nounahon.j@gmail.com>
2017-11-24 15:12:10 +0100

    Update crowdsale.som

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,1 +1,4 @@
+/// @title Solidity Smart-contract in order to manage a Crowdsale ICO
+/// @author Jean-Mickael Nounahon - <nounahon.j@gmail.com>
+/// @author Hugo Brunet- <Hugo.brunet.j@devinci.fr>
 
